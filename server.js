23edae2814ae8bd0bcb0d894b36c8ba449bb31b8
manzav95 <zavalamanuel95@gmail.com>
2020-08-23 16:27:30 -0700

    added project to repo to deploy on GitHub and heroku/netlify"

---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socket = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const botName = 'ThizzCord Bot';
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// run wehn client connects
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+    //Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ThizzCord'));
+
+    //Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat.`)
+      );
+  });
+
+  //Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  //To all clients in general
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat.`)
+      );
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
